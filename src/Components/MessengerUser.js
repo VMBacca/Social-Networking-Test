a614758695d5dd573ab94f0071e0310347a12969
Vinícius Marcondes Bacca <vinimbacca@gmail.com>
2022-04-20 15:37:36 -0300

    add new css

--- src/Components/MessengerUser.js
+++ src/Components/MessengerUser.js
@@ -1,25 +1,18 @@
 import CircleImage from "./CircleImage";
 import Row from "./Styled/Row";
 import RegularText from "./Styled/RegularText";
+import "./MessengerUser.css";
 
 const MessengerUser = ({ img, active = false }) => {
   return (
-    <Row
-      style={{
-        margin: "10px 0",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
+    <Row id="host-messenger">
       <Row>
         <CircleImage image={img} active={active} />
         <RegularText style={{ fontWeight: "bold" }}>Random Name</RegularText>
       </Row>
       <div
+        className="messenger"
         style={{
-          height: 10,
-          width: 10,
-          borderRadius: 5,
           backgroundColor: active ? "green" : "#ffffff2a",
         }}
       ></div>
